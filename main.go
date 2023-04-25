2753d6e5c7f7f8b43091b721dda5d69b03610186
Haydn <haydnmey@gmail.com>
2023-04-25 15:08:16 +0200

    Created the updateVideoGame function to update a video game by id

--- main.go
+++ main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"log"
 	"encoding/json"
+	"fmt"
+	"log"
 	"math/rand"
+	"net/http"
 	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
 type VideoGame struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
-	Genre string `json:"genre"`
+	ID        string     `json:"id"`
+	Name      string     `json:"name"`
+	Genre     string     `json:"genre"`
 	Developer *Developer `json:"publisher"`
 }
 
 type Developer struct {
-	developerName string `json:"developername"`
-	publisherName string `json:"publishername"`
+	DeveloperName string `json:"developer_name"`
+	PublisherName string `json:"publisher_name"`
 }
 
 var games []VideoGame
@@ -60,13 +61,31 @@ func createVideoGame(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(game)
 }
 
+func updateVideoGame(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, game := range games {
+		// deleting the video game if it exists in the slice
+		if game.ID == params["id"] {
+			games = append(games[:index], games[index+1:]...)
+			// Add updated game body into the slice
+			var game VideoGame
+			_ = json.NewDecoder(r.Body).Decode(&game)
+			game.ID = params["id"]
+			games = append(games, game)
+			json.NewEncoder(w).Encode(game)
+			return
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
-	games = append(games, VideoGame{ID: "1", Name: "Destiny 2", Genre: "FPS/RPG", Developer: &Developer{developerName: "Bungie", publisherName: "Bungie"}})
-	games = append(games, VideoGame{ID: "2", Name: "Call of Duty: Black ops 3", Genre: "FPS", Developer: &Developer{developerName: "Activision", publisherName: "Treyarch"}})
-	games = append(games, VideoGame{ID: "1", Name: "Witcher 3", Genre: "RPG", Developer: &Developer{developerName: "CD Projekt RED", publisherName: "CD Project"}})
-	
+	games = append(games, VideoGame{ID: "1", Name: "Destiny 2", Genre: "FPS/RPG", Developer: &Developer{DeveloperName: "Bungie", PublisherName: "Bungie"}})
+	games = append(games, VideoGame{ID: "2", Name: "Call of Duty: Black ops 3", Genre: "FPS", Developer: &Developer{DeveloperName: "Activision", PublisherName: "Treyarch"}})
+	games = append(games, VideoGame{ID: "1", Name: "Witcher 3", Genre: "RPG", Developer: &Developer{DeveloperName: "CD Projekt RED", PublisherName: "CD Project"}})
+
 	r.HandleFunc("/videogames", getVideoGames).Methods("GET")
 	r.HandleFunc("/videogames/{id}", getVideoGame).Methods("GET")
 	r.HandleFunc("/videogames", createVideoGame).Methods("POST")
